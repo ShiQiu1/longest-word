17c137937f036f88d9e92333277183fa62a66dc0
Shi Qiu <shi.qiu.773@gmail.com>
2024-06-06 17:28:08 +0000

    Feature complete: Dictionary check of attempt

--- tests/test_game.py
+++ tests/test_game.py
@@ -15,15 +15,17 @@ class TestGame:
         new_game = Game()
         test_grid = "OQUWRBAZE"
         test_longest_word = "BAROQUE"
-        test_failed_word = "bower" # lower case. Only test if the letters are within grid
+        test_failed_word = "bowerv" # lower case. Only test if the letters are within grid
         new_game.grid = list(test_grid)
-        assert new_game.is_valid(test_longest_word) == True, "The longest word is not right"
+        assert new_game.is_valid(test_longest_word) is True, "The longest word is not right"
         assert new_game.grid == list(test_grid), "Make sure the grid does not get modified"
-        assert new_game.is_valid(test_failed_word) == False, "This word is not the longest"
-        assert new_game.is_valid("") == False, "Empty word is not allowed"
+        assert new_game.is_valid(test_failed_word) is False, "Contain letter outside the grid"
+        assert new_game.is_valid("") is False, "Empty word is not allowed"
 
     def test_unknown_word_is_invalid(self):
         """A word that is not in the English dictionary should not be valid"""
         new_game = Game()
-        new_game.grid = list('KWIENFUQW') # Force the grid to a test case:
-        assert new_game.is_valid('FEUN') is False
+        test_grid  = 'KWIENFUQW'
+        new_game.grid = list(test_grid) # Force the grid to a test case:
+        assert new_game.is_valid('FEUN') is False, "FEUN is not a real word"
+        assert new_game.is_valid('FUN') is True, "Fun is a real word"
